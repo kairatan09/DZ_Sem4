40bd5179f25870a55519562306a376b2aa18d86f
Kairat Nagmetov <kairatan09@gmail.com>
2023-02-26 13:41:03 +0500

    Task27

---
+++ Task-27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Welcome(string message)
+{
+  Console.Write(message);
+  int number = Convert.ToInt32(Console.ReadLine());
+  return number;
+}
+
+void Sum(int number)
+{
+  int temp = number;
+  if (number > 0)
+  {
+    int res = 0;
+    for (int i = 0; number > 0; i++)
+    {
+      res += number % 10;
+      number /= 10;
+    }
+    Console.WriteLine($"\nСумма цифр числа {temp} равно: \t{res}");
+  }
+  else
+    Console.WriteLine("Введенное число не натуральное!");
+}
+
+try
+{
+  int number = Welcome("Введите натуральное число:\t");
+  Sum(number);
+}
+catch
+{
+  Console.WriteLine("Были введены некорректные данные");
+}
